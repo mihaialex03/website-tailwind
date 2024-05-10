61f356eef594f88c05845d6f04ef3d43bfd93e5f
Alexandru Mihai <alexandrumihai.03@icloud.com>
2024-05-10 11:59:54 +0300

    small changes added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,31 @@
 import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
+import useCartStore from '../store/useCartStore'; 
 
 export const Contact = () => {
   const form = useRef();
   const [showNotification, setShowNotification] = useState(false);
   const [notificationMessage, setNotificationMessage] = useState('');
   const [notificationType, setNotificationType] = useState('');
+
+  const { cartItems } = useCartStore();
+
+  const [formData, setFormData] = useState({
+    user_name: '',
+    user_email: '',
+    message: ''
+  });
+
+  useEffect(() => {
+    
+    if (cartItems.length > 0) {
+      const itemName = cartItems.map(item => item.name).join(', ');
+      setFormData(prevState => ({
+        ...prevState,
+        message: `I'm interested in the following program: ${itemName}`
+      }));
+    }
+  }, [cartItems]);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -30,6 +50,14 @@ export const Contact = () => {
       );
   };
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
   useEffect(() => {
     if (showNotification) {
       const timeoutId = setTimeout(() => {
@@ -47,15 +75,15 @@ export const Contact = () => {
         <form ref={form} onSubmit={sendEmail} className="max-w-md mx-auto">
           <div className="mb-4">
             <label className="block mb-2 text-primary">Name</label>
-            <input type="text" name="user_name" className="w-full border border-primary rounded-md px-4 py-2" required />
+            <input type="text" name="user_name" value={formData.user_name} onChange={handleInputChange} className="w-full border border-primary rounded-md px-4 py-2" required />
           </div>
           <div className="mb-4">
             <label className="block mb-2 text-primary">Email</label>
-            <input type="email" name="user_email" className="w-full border border-primary rounded-md px-4 py-2" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" />
+            <input type="email" name="user_email" value={formData.user_email} onChange={handleInputChange} className="w-full border border-primary rounded-md px-4 py-2" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" />
           </div>
           <div className="mb-4">
             <label className="block mb-2 text-primary">Message</label>
-            <textarea name="message" className="w-full border border-primary rounded-md px-4 py-2"></textarea>
+            <textarea name="message" value={formData.message} onChange={handleInputChange} className="w-full border border-primary rounded-md px-4 py-2"></textarea>
           </div>
           <button type="submit" className="bg-primary text-background px-4 py-2 rounded-md hover:bg-accent mx-auto block">Send</button>
         </form>
